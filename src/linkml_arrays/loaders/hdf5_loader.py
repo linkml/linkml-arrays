c8f1471013d98ff620110d9c6eac508a838c4a30
rly <rly@lbl.gov>
2024-02-04 23:39:13 -0800

    Add docstrings, fix flake8 issues

--- src/linkml_arrays/loaders/hdf5_loader.py
+++ src/linkml_arrays/loaders/hdf5_loader.py
@@ -1,3 +1,4 @@
+"""Class for loading a LinkML model from an HDF5 file."""
 from typing import Type, Union
 
 import h5py
@@ -8,9 +9,13 @@ from linkml_runtime.utils.yamlutils import YAMLRoot
 from pydantic import BaseModel
 
 
-def iterate_element(
+def _iterate_element(
     group: h5py.Group, element_type: ClassDefinition, schemaview: SchemaView
 ) -> dict:
+    """Recursively iterate through the elements of a LinkML model and load them into a dict.
+
+    Datasets are read into memory.
+    """
     ret_dict = dict()
     for k, v in group.attrs.items():
         ret_dict[k] = v
@@ -21,10 +26,10 @@ def iterate_element(
         )  # assumes the slot name has been written as the name which is OK for now.
         if "linkml:elements" in found_slot.implements:
             assert isinstance(v, h5py.Dataset)
-            v = v[()]  # read all the values into memory
+            v = v[()]  # read all the values into memory TODO: support lazy loading
         elif isinstance(v, h5py.Group):  # it's a subgroup
             found_slot_range = schemaview.get_class(found_slot.range)
-            v = iterate_element(v, found_slot_range, schemaview)
+            v = _iterate_element(v, found_slot_range, schemaview)
         # else: do not transform v
         ret_dict[k] = v
 
@@ -32,13 +37,14 @@ def iterate_element(
 
 
 class Hdf5Loader(Loader):
+    """Class for loading a LinkML model from an HDF5 file."""
 
     def load_any(self, source: str, **kwargs):
-        """Return element formatted as a YAML string with the path to an HDF5 file"""
+        """Create an instance of the target class from an HDF5 file."""
         return self.load(source, **kwargs)
 
     def loads(self, source: str, **kwargs):
-        """Return element formatted as a YAML string with the path to an HDF5 file"""
+        """Create an instance of the target class from an HDF5 file."""
         return self.load(source, **kwargs)
 
     def load(
@@ -48,10 +54,10 @@ class Hdf5Loader(Loader):
         schemaview: SchemaView,
         **kwargs,
     ):
-        """Return element formatted as a YAML string with the path to an HDF5 file"""
+        """Create an instance of the target class from an HDF5 file."""
         element_type = schemaview.get_class(target_class.__name__)
         with h5py.File(source, "r") as f:
-            element = iterate_element(f, element_type, schemaview)
+            element = _iterate_element(f, element_type, schemaview)
         obj = target_class(**element)
 
         return obj
