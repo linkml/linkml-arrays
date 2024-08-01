0858c1751ef4f516d9c7ef2ed3479dfb1a79d5a7
Wouter-Michiel Vierdag <michiel.vierdag@embl.de>
2024-08-01 11:29:59 +0200

    update module imports

--- src/linkml_arrays/dumpers/__init__.py
+++ src/linkml_arrays/dumpers/__init__.py
@@ -1,15 +1,20 @@
 """Dumper classes for linkml-arrays."""
 
 from .hdf5_dumper import Hdf5Dumper
+from .xarray_dumpers import XarrayNetCDFDumper, XarrayZarrDumper
 from .yaml_dumper import YamlDumper
 from .yaml_hdf5_dumper import YamlHdf5Dumper
 from .yaml_numpy_dumper import YamlNumpyDumper
+from .yaml_xarray_dumper import YamlXarrayNetCDFDumper
 from .zarr_directory_store_dumper import ZarrDirectoryStoreDumper
 
 __all__ = [
     "Hdf5Dumper",
+    "XarrayNetCDFDumper",
+    "XarrayZarrDumper",
     "YamlDumper",
     "YamlHdf5Dumper",
     "YamlNumpyDumper",
     "ZarrDirectoryStoreDumper",
+    "YamlXarrayNetCDFDumper",
 ]
