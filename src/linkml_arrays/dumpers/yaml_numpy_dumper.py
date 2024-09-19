bacaf30265d2087cc18f00dc3c9caec659662ba3
Ryan Ly <rly@lbl.gov>
2024-09-19 09:20:28 -0700

    Update to work with new array schema and LoL pydantic generator (#14)

--- src/linkml_arrays/dumpers/yaml_numpy_dumper.py
+++ src/linkml_arrays/dumpers/yaml_numpy_dumper.py
@@ -1,67 +1,34 @@
-"""Class for dumpling a LinkML model to a YAML file with paths to NumPy files."""
+"""Class for dumping a LinkML model to YAML with paths to NumPy files."""
 
-from typing import Union
+from pathlib import Path
+from typing import List, Union
 
 import numpy as np
-import yaml
-from linkml_runtime import SchemaView
-from linkml_runtime.dumpers.dumper_root import Dumper
-from linkml_runtime.utils.yamlutils import YAMLRoot
-from pydantic import BaseModel
+
+from .yaml_array_file_dumper import YamlArrayFileDumper
 
 
-def _iterate_element(
-    element: Union[YAMLRoot, BaseModel], schemaview: SchemaView, parent_identifier=None
-):
-    """Recursively iterate through the elements of a LinkML model and save them.
+class YamlNumpyDumper(YamlArrayFileDumper):
+    """Dumper class for LinkML models to YAML with paths to .npy files, one per array.
 
-    Returns a dictionary with the same structure as the input element, but with the slots
-    that implement "linkml:elements" (arrays) are written to HDF5 files and the paths to these
-    files are returned in the dictionary. Each array is written to an HDF5 dataset at path
-    "/data" in a new HDF5 file.
-
-    Raises:
-        ValueError: If the class requires an identifier and it is not provided.
+    Each array is written to an HDF5 dataset at path "/data" in a new HDF5 file.
     """
-    # get the type of the element
-    element_type = type(element).__name__
 
-    # ask schemaview whether it has a class by this name
-    found_class = schemaview.get_class(element_type)
+    FILE_SUFFIX = ".npy"  # used in parent class
+    FORMAT = "numpy"
 
-    id_slot = schemaview.get_identifier_slot(found_class.name)
-    if id_slot is not None:
-        id_value = getattr(element, id_slot.name)
-    else:
-        id_value = None
-
-    ret_dict = dict()
-    for k, v in vars(element).items():
-        found_slot = schemaview.induced_slot(k, element_type)
-        if "linkml:elements" in found_slot.implements:
-            if id_slot is None and parent_identifier is None:
-                raise ValueError("The class requires an identifier.")
-            # save the numpy array to file
-            if parent_identifier is not None:
-                output_file_path = f"{parent_identifier}.{found_class.name}.{found_slot.name}.npy"
-            else:
-                output_file_path = f"{found_class.name}.{found_slot.name}.npy"
-            np.save(output_file_path, v)  # TODO do not assume that there is only one by this name
-            ret_dict[k] = f"file:./{output_file_path}"  # TODO make this nicer
-        else:
-            if isinstance(v, BaseModel):
-                v2 = _iterate_element(v, schemaview, id_value)
-                ret_dict[k] = v2
-            else:
-                ret_dict[k] = v
-    return ret_dict
-
-
-class YamlNumpyDumper(Dumper):
-    """Dumper class for LinkML models to YAML files with paths to NumPy files."""
-
-    def dumps(self, element: Union[YAMLRoot, BaseModel], schemaview: SchemaView, **kwargs) -> str:
-        """Return element formatted as a YAML string."""
-        input = _iterate_element(element, schemaview)
-
-        return yaml.dump(input)
+    @classmethod
+    def write_array(
+        cls, array: Union[List, np.ndarray], output_file_path_no_suffix: Union[str, Path]
+    ):
+        """Write an array to a NumPy file."""
+        # TODO do not assume that there is only one by this name
+        # add suffix to the file name
+        if isinstance(output_file_path_no_suffix, str):
+            output_file_path_no_suffix = Path(output_file_path_no_suffix)
+        output_file_path = output_file_path_no_suffix.parent / (
+            output_file_path_no_suffix.name + cls.FILE_SUFFIX
+        )
+        arr = np.array(array)
+        np.save(output_file_path, arr)
+        return output_file_path
--- tests/test_dumpers/test_dumpers.py
+++ tests/test_dumpers/test_dumpers.py
@@ -1,11 +1,13 @@
-"""Test dumping LinkML models in pydantic-style classes to various file formats."""
+"""Test dumping LinkML pydantic models with arrays as lists-of-lists to various file formats."""
 
 import os
-import unittest
 from pathlib import Path
 
+import h5py
 import numpy as np
+import zarr
 from linkml_runtime import SchemaView
+from ruamel.yaml import YAML
 
 from linkml_arrays.dumpers import (
     Hdf5Dumper,
@@ -14,183 +16,154 @@ from linkml_arrays.dumpers import (
     YamlNumpyDumper,
     ZarrDirectoryStoreDumper,
 )
-from tests.test_dumpers.array_classes import (
-    DaySeries,
-    LatitudeSeries,
-    LongitudeSeries,
+from tests.array_classes_lol import (
+    Container,
+    DateSeries,
+    DaysInDSinceSeries,
+    LatitudeInDegSeries,
+    LongitudeInDegSeries,
     TemperatureDataset,
-    TemperatureMatrix,
+    TemperaturesInKMatrix,
 )
 
+INPUT_DIR = Path(__file__).parent.parent / "input"
 
-class YamlDumpersTestCase(unittest.TestCase):
-    """Test dumping of pydantic-style classes from LinkML schemas into YAML files."""
 
-    def test_dump_pydantic_arrays(self):
-        """Test dumping pydantic classes with numpy arrays to a YAML file."""
-        latitude_in_deg = LatitudeSeries(values=np.array([1, 2]))
-        longitude_in_deg = LongitudeSeries(values=np.array([4, 5]))
-        time_in_d = DaySeries(values=np.array([7, 8]))
-        temperatures_in_K = TemperatureMatrix(
-            values=np.arange(8).reshape((2, 2, 2)),
+def _create_container() -> Container:
+    latitude_in_deg = LatitudeInDegSeries(name="my_latitude", values=[[1, 2], [3, 4]])
+    longitude_in_deg = LongitudeInDegSeries(name="my_longitude", values=[[5, 6], [7, 8]])
+    date = DateSeries(values=["2020-01-01", "2020-01-02"])
+    days_in_d_since = DaysInDSinceSeries(values=[0, 1], reference_date="2020-01-01")
+    temperatures_in_K = TemperaturesInKMatrix(
+        conversion_factor=1000.0,
+        values=[[[0, 1], [2, 3]], [[4, 5], [6, 7]]],
+    )
+    # NOTE: currently no way to pass in the actual LatitudeInDegSeries object
+    temperature_dataset = TemperatureDataset(
+        name="my_temperature",
+        latitude_in_deg="my_latitude",
+        longitude_in_deg="my_longitude",
+        date=date,
+        day_in_d=days_in_d_since,
+        temperatures_in_K=temperatures_in_K,
+    )
+    container = Container(
+        name="my_container",
+        latitude_series=latitude_in_deg,
+        longitude_series=longitude_in_deg,
+        temperature_dataset=temperature_dataset,
+    )
+
+    return container
+
+
+def test_yaml_dumper():
+    """Test YamlDumper dumping to a YAML file."""
+    container = _create_container()
+
+    schemaview = SchemaView(INPUT_DIR / "temperature_schema.yaml")
+    ret = YamlDumper().dumps(container, schemaview=schemaview)
+
+    # read and compare with the expected YAML file ignoring order of keys
+    expected_yaml_file = INPUT_DIR / "container_yaml.yaml"
+    yaml = YAML(typ="safe")
+    with open(expected_yaml_file) as f:
+        expected = yaml.load(f)  # load yaml into dictionary
+        actual = yaml.load(ret)
+        assert actual == expected
+
+
+def test_yaml_numpy_dumper():
+    """Test YamlNumpyDumper dumping to a YAML file and NumPy .npy files in a directory."""
+    container = _create_container()
+
+    schemaview = SchemaView(INPUT_DIR / "temperature_schema.yaml")
+    ret = YamlNumpyDumper().dumps(container, schemaview=schemaview, output_dir="./out")
+
+    # read and compare with the expected YAML file ignoring order of keys
+    expected_yaml_file = INPUT_DIR / "container_yaml_numpy.yaml"
+    yaml = YAML(typ="safe")
+    with open(expected_yaml_file) as f:
+        expected = yaml.load(f)  # load yaml into dictionary
+        actual = yaml.load(ret)
+        assert actual == expected
+
+
+def test_yaml_hdf5_dumper():
+    """Test YamlNumpyDumper dumping to a YAML file and HDF5 datasets in a directory."""
+    container = _create_container()
+
+    schemaview = SchemaView(INPUT_DIR / "temperature_schema.yaml")
+    ret = YamlHdf5Dumper().dumps(container, schemaview=schemaview, output_dir="./out")
+
+    # read and compare with the expected YAML file ignoring order of keys
+    expected_yaml_file = INPUT_DIR / "container_yaml_hdf5.yaml"
+    yaml = YAML(typ="safe")
+    with open(expected_yaml_file) as f:
+        expected = yaml.load(f)  # load yaml into dictionary
+        actual = yaml.load(ret)
+        assert actual == expected
+
+
+def test_hdf5_dumper(tmp_path):
+    """Test Hdf5Dumper dumping to an HDF5 file."""
+    container = _create_container()
+
+    schemaview = SchemaView(INPUT_DIR / "temperature_schema.yaml")
+    output_file_path = tmp_path / "my_container.h5"
+    Hdf5Dumper().dumps(container, schemaview=schemaview, output_file_path=output_file_path)
+
+    assert os.path.exists(output_file_path)
+    with h5py.File(output_file_path, "r") as f:
+        assert f.attrs["name"] == "my_container"
+        assert set(f["latitude_series"].keys()) == set(["values"])
+        np.testing.assert_array_equal(f["latitude_series/values"][:], [[1, 2], [3, 4]])
+        assert set(f["longitude_series"].keys()) == set(["values"])
+        np.testing.assert_array_equal(f["longitude_series/values"][:], [[5, 6], [7, 8]])
+        assert set(f["temperature_dataset"]) == set(["date", "day_in_d", "temperatures_in_K"])
+        assert set(f["temperature_dataset/date"].keys()) == set(["values"])
+        np.testing.assert_array_equal(
+            f["temperature_dataset/date/values"].asstr()[:], np.array(["2020-01-01", "2020-01-02"])
         )
-        temperature = TemperatureDataset(
-            name="my_temperature",
-            latitude_in_deg=latitude_in_deg,
-            longitude_in_deg=longitude_in_deg,
-            time_in_d=time_in_d,
-            temperatures_in_K=temperatures_in_K,
-        )
-
-        schemaview = SchemaView(Path(__file__) / "../../input/temperature_dataset.yaml")
-        ret = YamlDumper().dumps(temperature, schemaview=schemaview)
-
-        expected = """latitude_in_deg:
-  values:
-  - 1
-  - 2
-longitude_in_deg:
-  values:
-  - 4
-  - 5
-name: my_temperature
-temperatures_in_K:
-  values:
-  - - - 0
-      - 1
-    - - 2
-      - 3
-  - - - 4
-      - 5
-    - - 6
-      - 7
-time_in_d:
-  values:
-  - 7
-  - 8
-"""
-        assert ret == expected
-
-
-class YamlNumpyDumpersTestCase(unittest.TestCase):
-    """Test dumping of pydantic-style classes from LinkML schemas into YAML + NumPy files."""
-
-    def test_dump_pydantic_arrays(self):
-        """Test dumping pydantic classes with numpy arrays to a YAML + NumPy files."""
-        latitude_in_deg = LatitudeSeries(values=np.array([1, 2, 3]))
-        longitude_in_deg = LongitudeSeries(values=np.array([4, 5, 6]))
-        time_in_d = DaySeries(values=np.array([7, 8, 9]))
-        temperatures_in_K = TemperatureMatrix(
-            values=np.ones((3, 3, 3)),
-        )
-        temperature = TemperatureDataset(
-            name="my_temperature",
-            latitude_in_deg=latitude_in_deg,
-            longitude_in_deg=longitude_in_deg,
-            time_in_d=time_in_d,
-            temperatures_in_K=temperatures_in_K,
-        )
-
-        schemaview = SchemaView(Path(__file__) / "../../input/temperature_dataset.yaml")
-        ret = YamlNumpyDumper().dumps(temperature, schemaview=schemaview)
-
-        expected = """latitude_in_deg:
-  values: file:./my_temperature.LatitudeSeries.values.npy
-longitude_in_deg:
-  values: file:./my_temperature.LongitudeSeries.values.npy
-name: my_temperature
-temperatures_in_K:
-  values: file:./my_temperature.TemperatureMatrix.values.npy
-time_in_d:
-  values: file:./my_temperature.DaySeries.values.npy
-"""
-        assert ret == expected
-
-
-class YamlHdf5DumpersTestCase(unittest.TestCase):
-    """Test dumping of pydantic-style classes from LinkML schemas into YAML + HDF5 datasets."""
-
-    def test_dump_pydantic_arrays(self):
-        """Test dumping pydantic classes with numpy arrays to a YAML + HDF5 datasets."""
-        latitude_in_deg = LatitudeSeries(values=np.array([1, 2, 3]))
-        longitude_in_deg = LongitudeSeries(values=np.array([4, 5, 6]))
-        time_in_d = DaySeries(values=np.array([7, 8, 9]))
-        temperatures_in_K = TemperatureMatrix(
-            values=np.ones((3, 3, 3)),
-        )
-        temperature = TemperatureDataset(
-            name="my_temperature",
-            latitude_in_deg=latitude_in_deg,
-            longitude_in_deg=longitude_in_deg,
-            time_in_d=time_in_d,
-            temperatures_in_K=temperatures_in_K,
-        )
-
-        schemaview = SchemaView(Path(__file__) / "../../input/temperature_dataset.yaml")
-        ret = YamlHdf5Dumper().dumps(temperature, schemaview=schemaview)
-
-        expected = """latitude_in_deg:
-  values: file:./my_temperature.LatitudeSeries.values.h5
-longitude_in_deg:
-  values: file:./my_temperature.LongitudeSeries.values.h5
-name: my_temperature
-temperatures_in_K:
-  values: file:./my_temperature.TemperatureMatrix.values.h5
-time_in_d:
-  values: file:./my_temperature.DaySeries.values.h5
-"""
-        assert ret == expected
-
-
-class Hdf5DumpersTestCase(unittest.TestCase):
-    """Test dumping of pydantic-style classes from LinkML schemas into a single HDF5 file."""
-
-    def test_dump_pydantic_arrays(self):
-        """Test dumping pydantic classes with numpy arrays to a signle HDF5 file."""
-        latitude_in_deg = LatitudeSeries(values=np.array([1, 2, 3]))
-        longitude_in_deg = LongitudeSeries(values=np.array([4, 5, 6]))
-        time_in_d = DaySeries(values=np.array([7, 8, 9]))
-        temperatures_in_K = TemperatureMatrix(
-            values=np.ones((3, 3, 3)),
-        )
-        temperature = TemperatureDataset(
-            name="my_temperature",
-            latitude_in_deg=latitude_in_deg,
-            longitude_in_deg=longitude_in_deg,
-            time_in_d=time_in_d,
-            temperatures_in_K=temperatures_in_K,
+        assert set(f["temperature_dataset/day_in_d"].keys()) == set(["values"])
+        assert f["temperature_dataset/day_in_d"].attrs["reference_date"] == "2020-01-01"
+        np.testing.assert_array_equal(f["temperature_dataset/day_in_d/values"][:], [0, 1])
+        assert set(f["temperature_dataset/temperatures_in_K"].keys()) == set(["values"])
+        np.testing.assert_array_equal(
+            f["temperature_dataset/temperatures_in_K/values"][:],
+            [[[0, 1], [2, 3]], [[4, 5], [6, 7]]],
         )
 
-        schemaview = SchemaView(Path(__file__) / "../../input/temperature_dataset.yaml")
-        Hdf5Dumper().dumps(temperature, schemaview=schemaview)
 
-        assert os.path.exists("my_temperature.h5")
+def test_zarr_directory_store_dumper(tmp_path):
+    """Test ZarrDumper dumping to an HDF5 file."""
+    container = _create_container()
 
-        # TODO use h5py to assert that the data is correct
+    schemaview = SchemaView(INPUT_DIR / "temperature_schema.yaml")
+    output_file_path = tmp_path / "my_container.zarr"
+    ZarrDirectoryStoreDumper().dumps(
+        container, schemaview=schemaview, output_file_path=output_file_path
+    )
 
+    assert os.path.exists(output_file_path)
 
-class ZarrDirectoryStoreDumpersTestCase(unittest.TestCase):
-    """Test dumping of pydantic-style classes from LinkML schemas into a Zarr directory store."""
-
-    def test_dump_pydantic_arrays(self):
-        """Test dumping pydantic classes with numpy arrays to a Zarr directory store."""
-        latitude_in_deg = LatitudeSeries(values=np.array([1, 2, 3]))
-        longitude_in_deg = LongitudeSeries(values=np.array([4, 5, 6]))
-        time_in_d = DaySeries(values=np.array([7, 8, 9]))
-        temperatures_in_K = TemperatureMatrix(
-            values=np.ones((3, 3, 3)),
-        )
-        temperature = TemperatureDataset(
-            name="my_temperature",
-            latitude_in_deg=latitude_in_deg,
-            longitude_in_deg=longitude_in_deg,
-            time_in_d=time_in_d,
-            temperatures_in_K=temperatures_in_K,
-        )
-
-        schemaview = SchemaView(Path(__file__) / "../../input/temperature_dataset.yaml")
-        ZarrDirectoryStoreDumper().dumps(temperature, schemaview=schemaview)
-
-        assert os.path.exists("my_temperature.zarr")
-
-        # TODO use Zarr to assert that the data is correct
+    root = zarr.group(store=output_file_path)
+    # NOTE this is pretty much the same code as test_hdf5_dumper
+    assert root.attrs["name"] == "my_container"
+    assert set(root["latitude_series"].keys()) == set(["values"])
+    np.testing.assert_array_equal(root["latitude_series/values"][:], [[1, 2], [3, 4]])
+    assert set(root["longitude_series"].keys()) == set(["values"])
+    np.testing.assert_array_equal(root["longitude_series/values"][:], [[5, 6], [7, 8]])
+    assert set(root["temperature_dataset"]) == set(["date", "day_in_d", "temperatures_in_K"])
+    assert set(root["temperature_dataset/date"].keys()) == set(["values"])
+    np.testing.assert_array_equal(
+        root["temperature_dataset/date/values"][:], np.array(["2020-01-01", "2020-01-02"])
+    )
+    assert set(root["temperature_dataset/day_in_d"].keys()) == set(["values"])
+    assert root["temperature_dataset/day_in_d"].attrs["reference_date"] == "2020-01-01"
+    np.testing.assert_array_equal(root["temperature_dataset/day_in_d/values"][:], [0, 1])
+    assert set(root["temperature_dataset/temperatures_in_K"].keys()) == set(["values"])
+    np.testing.assert_array_equal(
+        root["temperature_dataset/temperatures_in_K/values"][:],
+        [[[0, 1], [2, 3]], [[4, 5], [6, 7]]],
+    )
